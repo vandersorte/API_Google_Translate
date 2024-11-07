62ea6bc7108c7878c85b702892bab9cfd6da1c19
vander <vander.3601@hotmail.com>
2024-11-07 10:25:57 -0300

    modificações

--- traducao.py
+++ traducao.py
@@ -1,11 +1,11 @@
 from deep_translator import GoogleTranslator
 
-text_en = 'Starting a company is like throwing yourself off the cliff and assembling an airplane on the way down.'
+text_in_English = 'Starting a company is like throwing yourself off the cliff and assembling an airplane on the way down.'
 
-def traduzir(text):
-  tradutor = GoogleTranslator(source='en', target='pt')
-  traducao = tradutor.translate(text)
-  return traducao
+def translate_function(text):
+  translator = GoogleTranslator(source='en', target='pt')
+  translated_text = translator.translate(text)
+  return translated_text
 
-print(traduzir(text_en))
+print(translate_function(text_in_English))
 
